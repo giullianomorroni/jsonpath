21edcd7d316cdfb862ee2a9a4b433ccaccca1444
giulliano morroni <giullianomorroni@gmail.com>
2013-03-31 23:59:06 -0300

    brasil pt-BR

--- jpath/jpath/settings.py
+++ jpath/jpath/settings.py
@@ -28,11 +28,14 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Sao_Paulo'
+#'America/Chicago'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR' 
+#'en-us'
+
 
 SITE_ID = 1
 
